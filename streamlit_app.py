758d5820c08ab20266740ebbd03c6c82d5d34ccf
coletistan <coletistan@gmail.com>
2022-09-23 15:59:40 -0400

    Added derivative and integral selectbox and user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,32 @@ import sympy as sp
 from sympy.plotting import plot
 
 
-# set symbols for calculations
+# set constants for sympy and streamlit
+options = ('Derivative', 'Integral')
+
 x, y = sp.symbols('x y')
-sp.init_printing()
-gfg_exp = sp.csc(x)
+sp.init_printing(use_latex=True)
 
-st.latex("Before Differentiation")
-st.latex(sp.latex(gfg_exp))
-  
-# Use sympy.diff() method to differentiate expression
-dif = sp.diff(gfg_exp, x)
+user_input = st.text_input(
+    'Enter expression here... Ex: x**2',
+    x**2,
+    placeholder=x**2
+)
 
-st.latex("After Differentiation")
-st.latex(dif)
+select_box = st.selectbox(
+    'Would you like to visualize a derivative or integral?',
+    options
+)
+
+st.write('You selected:', select_box)
+
+if select_box == 'Derivative':
+    # Use sympy.diff() method to differentiate expression
+    dif = sp.diff(user_input, x)
+
+    st.latex("Differentiation")
+    st.latex(dif)
+else:
+    inte = sp.integrate(user_input, x)
+    st.latex("Integral")
+    st.latex(inte)
