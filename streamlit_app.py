880690d63c1830242f16f194eac664e1f0693343
coletistan <coletistan@gmail.com>
2022-09-26 18:18:59 -0400

    Added streamlit pyplot to visualize expressions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,52 @@
 import streamlit as st
-
 import sympy as sp
+
 from sympy.plotting import plot
 
 
-# set constants for sympy and streamlit
-options = ('Derivative', 'Integral')
+def get_figure(user_input, expr):
+    """
+    Retrieves matplotlib figure
 
-x, y = sp.symbols('x y')
-sp.init_printing(use_latex=True)
+    Args:
+        user_input (String): input by user
+        expr (String): expression being evaluated
 
-user_input = st.text_input(
-    'Enter expression here... Ex: x**2',
-    x**2,
-    placeholder=x**2
-)
+    Returns:
+        Plot: return Plot object
+    """
+    sp_plot = plot(user_input, expr, (x, -1, 1))
+    return sp_plot._backend.fig
 
-select_box = st.selectbox(
-    'Would you like to visualize a derivative or integral?',
-    options
-)
 
-if select_box == 'Derivative':
-    # Use sympy.diff() method to differentiate expression
-    dif = sp.diff(user_input, x)
-    st.latex("Differentiation")
-    st.latex(dif)
-else:
-    inte = sp.integrate(user_input, x)
-    st.latex("Integral")
-    st.latex(inte)
+if __name__ == '__main__':
+
+    # set constants for sympy and streamlit
+    options = ('Derivative', 'Integral')
+
+    x = sp.symbols('x')
+    sp.init_printing(use_latex=True)
+
+    user_input = st.text_input(
+        'Enter expression here... Ex: x**2',
+        x**2,
+        placeholder=x**2
+    )
+
+    select_box = st.selectbox(
+        'Would you like to visualize a derivative or integral?',
+        options
+    )
+
+    if select_box == 'Derivative':
+        # Use sympy.diff() method to differentiate expression
+        dif = sp.diff(user_input, x)
+        st.latex("Differentiation")
+        st.latex(dif)
+        st.pyplot(get_figure(user_input, dif))
+    else:
+        # use sympy.integrate to get the integral of the expression
+        inte = sp.integrate(user_input, x)
+        st.latex("Integral")
+        st.latex(inte)
+        st.pyplot(get_figure(user_input, inte))
